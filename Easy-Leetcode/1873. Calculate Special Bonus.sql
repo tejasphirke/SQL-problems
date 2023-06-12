3b3dac03fac52de35364ac05d643f1aaee4c1281
Tejas Phirke <41693514+tejasphirke@users.noreply.github.com>
2023-06-12 02:39:03 -0400

    using conditions on two columns and updating values in other columns

--- Easy-Leetcode/1873. Calculate Special Bonus.sql
+++ Easy-Leetcode/1873. Calculate Special Bonus.sql
@@ -14,3 +14,9 @@ SELECT employee_id,
        CASE WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary ELSE 0 END AS bonus
 FROM Employees
 ORDER BY 1
+
+### Solution using if condition
+select employee_id,
+if(employee_id%2=1 and name NOT REGEXP '^M', salary, 0) as bonus 
+from employees 
+order by employee_id;
