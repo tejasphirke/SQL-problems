0ce4d503dd73b7601150bfbe5656f0bc4005e0cc
Tejas Phirke <41693514+tejasphirke@users.noreply.github.com>
2023-06-12 02:35:55 -0400

    using conditions on two columns and updating values in other columns

--- Easy-Leetcode/1873. Calculate Special Bonus.sql
+++ Easy-Leetcode/1873. Calculate Special Bonus.sql
@@ -1,3 +1,4 @@
+### Solution 1
 SELECT employee_id,
 CASE 
   WHEN employee_id%2 = 0 THEN 0
@@ -7,3 +8,9 @@ CASE
 
 FROM Employees
 ORDER BY employee_id;
+
+### Solution 2
+SELECT employee_id, 
+       CASE WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary ELSE 0 END AS bonus
+FROM Employees
+ORDER BY 1
