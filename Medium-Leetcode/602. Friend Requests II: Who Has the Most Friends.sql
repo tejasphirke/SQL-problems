4be538c368d5890119b0fae094a04ff7fafe65f4
Tejas Phirke <41693514+tejasphirke@users.noreply.github.com>
2023-06-30 14:30:24 -0400

    friends

--- Medium-Leetcode/602. Friend Requests II: Who Has the Most Friends.sql
+++ Medium-Leetcode/602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,18 +1,20 @@
 # Solution 1
-select requester_id as id,
-       (select count(*) from RequestAccepted
-            where id=requester_id or id=accepter_id) as num
-from RequestAccepted
-group by requester_id
-order by num desc limit 1
+select id, count(id ) as num from
+(select requester_id as id from RequestAccepted
+union all
+select accepter_id as id from RequestAccepted)
+requestaccepted
+group by id
+order by num desc
+limit 1
 
 # Solution 2
 select id1 as id, count(id2) as num
 from
 (select requester_id as id1, accepter_id as id2 
-from request_accepted
+from requestaccepted
 union
 select accepter_id as id1, requester_id as id2 
-from request_accepted) tmp1
+from requestaccepted) tmp1
 group by id1 
 order by num desc limit 1
